32c69c8333ba5cc25e9e50528672974a73c75b81
boazfran <boazfr@gmail.com>
2020-03-04 17:02:34 +0200

    ongoing

--- define_clones.sh
+++ define_clones.sh
@@ -1,4 +1,6 @@
 
-AssignGenes.py igblast -s 1.fasta -b ~/share/igblast --organism human --loci ig --format blast
-MakeDb.py igblast -i 1_igblast.fmt7 -s 1.fasta -r ~/share/germlines/imgt/human/vdj IMGT_Human_IGHV.fasta IMGT_Human_IGHD.fasta IMGT_Human_IGHJ.fasta
-DefineClones.py -d 1_igblast_db-pass.tab
+for dir in GMC FV IB ; do
+	AssignGenes.py igblast -s $dir/full.fasta -b ~/share/igblast --organism human --loci ig --format blast --outdir $dir;
+	MakeDb.py igblast -i $dir/full_igblast.fmt7 -s $dir/full.fasta -r ~/share/germlines/imgt/human/vdj IMGT_Human_IGHV.fasta IMGT_Human_IGHD.fasta IMGT_Human_IGHJ.fasta --outdir $dir;
+	DefineClones.py -d $dir/full_igblast_db-pass.tab --outdir $dir;
+done
